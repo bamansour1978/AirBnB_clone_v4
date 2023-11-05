038f0bc749b85f90c995bc4e591abfe2aff93e86
bamansour1978 <karatman@gmail.com>
2023-11-05 20:39:25 +0100

    Update

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+b & o Flask App that integrates with AirBnB static HTML Template
 """
 import uuid
 from flask import Flask, render_template, url_for
